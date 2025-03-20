53eb3eb4e951b236a7f6d7564a02a8ebb43462f3
Chiranshu-Jindal <chiranshujindal03@GMAIL.COM>
2025-03-20 07:39:02 +0000

    comparisons

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+//these type of conversion create problems in code 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" == 2); // here it will perform conversion
+console.log("2" === 2);// here it goes under strict check as it stops conversion 
