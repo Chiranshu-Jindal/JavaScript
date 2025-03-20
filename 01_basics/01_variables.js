34da512ff213d81a01958733826482cd5ef0fa0b
Chiranshu-Jindal <chiranshujindal03@GMAIL.COM>
2025-03-20 07:03:53 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 837384
+var accountEmail="he@gmail.com"
+let accountPassword = "e098320"
+accountCity = "jaipur"
+let accountState;
+
+// accountId=2 //not allowed
+
+accountEmail="kjdshkjfs@skdjas.com"
+accountPassword="hey_83"
+accountCity="kenya"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
